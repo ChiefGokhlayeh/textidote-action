9dd5e3206d76fcdb1ec22b009edc0d7fe8caa0e9
Andreas Baulig <free.geronimo@hotmail.de>
2020-11-02 23:25:40 +0100

    Add threshold_error option to Action

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,8 @@ root_file="${1}"
 working_directory="${2}"
 report_type="${3}"
 report_file="${4}"
-args="${5}"
+threshold_error="${5}"
+args="${6}"
 
 IFS=' ' read -r -a args <<< "$args"
 
@@ -23,4 +24,16 @@ if [[ -n "$working_directory" ]]; then
   cd "$working_directory"
 fi
 
-exec /usr/local/bin/textidote --output "$report_type" "${args[@]}" "$root_file" > "$report_file"
+set +e
+
+/usr/local/bin/textidote --output "$report_type" "${args[@]}" "$root_file" > "$report_file"
+textidote_exit="$?"
+
+echo "Linter counted $textidote_exit warnings."
+
+set -e
+
+if [[ "$threshold_error" > -1 ]] && [[ "$threshold_error" < "$textidote_exit" ]]; then
+  echo "Linter warnings above error threshold ($textidote_exit > $threshold_error)!"
+  exit "$textidote_exit"
+fi
